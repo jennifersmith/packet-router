0f6e02b4462bb97693e39a402500be365ab76949
jennifersmith <jennifer@home>
2014-08-23 17:32:04 +1000

    Hooked in crafty and showed a background very similar the tutorial

--- src/packet_router/core.cljs
+++ src/packet_router/core.cljs
@@ -2,4 +2,10 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
+
+(defn start-game []
+  (.init js/Crafty 480 320)
+  (.background js/Crafty "rgb(87, 109, 20)")
+  (println "Wooooop"))
+
+(.addEventListener js/window "load" start-game)
