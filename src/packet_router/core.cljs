7d710a554a0cbb2f231e47ac699f4477bddf95ac
jennifersmith <jennifer@home>
2014-08-24 17:59:32 +1000

    Basic box bounds done in horrible way
    Now when packets try to go out of bounds, we have something to tell them
    not to. A series of polygons around the edge. Kinda gross. But it seems
    to work.
    
    If wanted to do the same with the ports too need to add on a little
    grace period for them to become free of the box - or change the point
    where they come out.

--- src/packet_router/core.cljs
+++ src/packet_router/core.cljs
@@ -135,7 +135,6 @@
 (defn init-moving []
   (this-as me
            (.requires me "2D")
-           (dump-statebag "I am a moving thing" :time (.-_time me) :vel (.-_velocity me))
            
            (.bind me "EnterFrame" #(update-position-of-moving-component me))
            )
@@ -170,11 +169,7 @@
            (let [vel (+ 10 (rand-int 40))
                  heading  (+ min-heading (rand-int (- max-heading min-heading)))
                  vector-velocity (map #(* % vel) (angle-to-unit-vectors (deg->rad heading)))]
-             (dump-statebag "moving randomly" 
-                            :vector-vels vector-velocity 
-                            :min-heading min-heading
-                            :max-heading max-heading
-                            :heading heading)
+
              (set!
               (.-_velocity me) vector-velocity))))
 
@@ -185,9 +180,11 @@
                           :moveRandomly move-randomly
                           }))
 
+(defn fetch-global-router-evil []
+  (js/Crafty (aget (js/Crafty "Router") 0)))
+
 (defn emit-packet [port]
   ;; eek
-  (dump-statebag "Emitting packet from port " :loc (.-loc port) :heading (.-heading port))
   (let [packet (make-entity "Packet")
         heading (.-heading port)
         entrance (.-entrance port)]
@@ -200,6 +197,11 @@
            (.delay me #(emit-packet me) 1000 1000)))
 
 
+(defn make-router-boundary [router x y w h]
+  (let [router-boundary (make-entity "RouterBoundary")]
+    (.attach router router-boundary)
+    (.attr  router-boundary (clj->js {:w w :x x :h h :y y}))))
+
 (defn init-router-component [] 
   (this-as me
            (.requires me "2D, Canvas, Color, Polygon")
@@ -207,15 +209,24 @@
            (.attr me (clj->js {"x" router-padding 
                                "y" router-padding 
                                "w" router-width
-                               "h" router-height}))))
+                               "h" router-height}))
+           (make-router-boundary me router-padding   router-padding  10 router-height )
+           (make-router-boundary me (+ router-width router-padding -10)  router-padding  10 router-height )
+           (make-router-boundary me router-padding  (- router-padding 10) router-width 10)
+           (make-router-boundary me router-padding  (+ router-padding router-height) router-width 10)))
 
+(defn stop-moving [packet]
+  (set!  (.-_velocity packet) [0 0])
+)
 (defn init-packet []
   (this-as me
-           (.requires me "2D, Canvas, Color, Polygon, RandomMover")
+           (.requires me "2D, Canvas, Color, Polygon, RandomMover, Collision, WiredHitBox")
            (.color me "rgb(100,0,0)")
            (.attr me (clj->js {:w 10 :h 10}))
 ;;           (.velocity me 1 1 0)
-           (.trigger js/Crafty "PacketCreated")))
+           (.trigger js/Crafty "PacketCreated")
+           (.onHit me "RouterBoundary" #(stop-moving me))
+           ))
 
 (make-component "Router" (clj->js {:init init-router-component
                                    }) )
@@ -226,6 +237,10 @@
            (.color me "rgb(100,100,100)")
            (.attr me (clj->js {:w 5 :h 6}))))
 
+(defn init-router-boundary []
+  (this-as me
+           (.requires me "2D, Polygon, Canvas, Collision, WiredHitBox, Color")
+           ))
 (make-component "Entrance" (clj->js {:init init-entrance}))
 
 (make-component "Port" (clj->js {:init init-port
@@ -234,9 +249,12 @@
 
 (make-component "Packet" (clj->js {:init init-packet}))
 
+(make-component "RouterBoundary" (clj->js {:init init-router-boundary}))
+
 (make-scene-with-transition "Intro" loading-scene "Game")
 (make-scene "Game" game-scene game-scene-uninit)
 (make-scene-with-transition "Finish" finish-scene "Intro")
+
 
 (defn start-game []
   (.init js/Crafty 480 320)
